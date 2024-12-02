802921b679efd0f92e92bf0eb1ef667fdf1a3b2d
Anthony Simond <pv22nbo@eduvaud.ch>
2024-12-02 11:28:29 +0100

    feat:add Puissance and float input Calculator

--- src/main.py
+++ src/main.py
@@ -1,25 +1,20 @@
-operand1 = float
+operand1 = None
 operator = None
-operand2 = float
+operand2 = None
 
 def main():
     ask_user_input()
-    result = calculate(float(), operator,float(operand2))
-    display_result(float(operand1), operator, float(operand2), float(result))
+    result = calculate(operand1, operator,operand2)
+    display_result(operand1, operator,operand2, result)
     ask_user_float_input(float)
 
-
-def ask_user_float_input():
-    global operand1
-    operand1 = ask_user_float_input("Enter the first operand: ")
-
-    global operator
-    # Get the operator from the user
-    operator = input("Enter an operator (+, -, *, /): ")
-
-    global operand2
-    # Get second operand from the user
-    operand2 = ask_user_float_input("Enter the second operand: ")
+def ask_user_float_input(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            return value
+        except ValueError:
+            print("Valeur invalide")
 
 
 def ask_user_input():
@@ -29,7 +24,7 @@ def ask_user_input():
 
     global operator
     # Get the operator from the user
-    operator = input("Enter an operator (+, -, *, /): ")
+    operator = input("Enter an operator (+, -, *, /, **): ")
 
     global operand2
     # Get second operand from the user
@@ -49,23 +44,19 @@ def calculate(ope1, oper, ope2):
                 print("Error: Division by zero is undefined.")
                 return
             res = ope1 / ope2
+        case '**':
+            res = ope1 ** ope2
+
         case _:
             print("Invalid operator.")
             return
     return res
- # Fonction puissance
-    def maFonction(n):
-        somme = 1
-        for count in range(int(n)):
-            somme = somme * 2
-        return somme
 
-    # Ce print sert à tester la fonction
-    print(maFonction(3))
+
 
 
 def display_result(op1, ope, ope2, res):
-    print(float(op1) + " " + ope + " " + float(ope2) + " = " + float(res))
+    print(str(op1) + " " + ope + " " + str(ope2) + " = " + str(res))
 
 
 # Call the main function to run the program
