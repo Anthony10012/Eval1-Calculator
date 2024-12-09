054945c1cb2dd8634407e804c61f42ec7adc7b8b
Anthony Simond <pv22nbo@eduvaud.ch>
2024-12-09 11:59:39 +0100

    feat: add impl 2

--- src/Mathlib.py
+++ src/Mathlib.py
@@ -3,8 +3,10 @@ from src.MathRequest import MathRequest
 class Mathlib:
     @classmethod
     def execute(self, mathRequest):
-        if (mathRequest.get_oper() == 'add'):
-            mathRequest.set_res(mathRequest.get_ope1() + mathRequest.get_ope2())
+        match mathRequest.get_oper() :
+            case 'add':
+                mathRequest.set_res(mathRequest.get_ope1() + mathRequest.get_ope2())
+
 
 
 
