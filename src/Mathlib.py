7353be18dc341dececa084db8e9cc73df08ef977
Anthony Simond <pv22nbo@eduvaud.ch>
2024-12-09 12:12:37 +0100

    feat: class Mathlib method add impl

--- src/Mathlib.py
+++ src/Mathlib.py
@@ -3,6 +3,10 @@ from src.MathRequest import MathRequest
 class Mathlib:
     @classmethod
     def execute(self, mathRequest):
+        oper= mathRequest.get_oper()
+        ope1 =mathRequest.get_ope1()
+        ope2 =mathRequest.get_ope2()
+
         match mathRequest.get_oper() :
             case 'add':
                 mathRequest.set_res(mathRequest.get_ope1() + mathRequest.get_ope2())
